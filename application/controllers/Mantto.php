74f5169c9752f63329f287c4d703501b62784eef
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-08-20 13:12:27 -0700

    Update Mantto

--- application/controllers/Mantto.php
+++ application/controllers/Mantto.php
@@ -1,43 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-<<<<<<< HEAD
-class Mannto extends CI_Controller{
-=======
+
+
 class Mantto extends CI_Controller{
->>>>>>> master
+
 
   public function __construct()
   {
     parent::__construct();
     //Codeigniter : Write Less Do More
-<<<<<<< HEAD
-  }
-
-  public function index()
-  {
-
-  }
-
-  public function Create($value='')
-  {
-    // code...
-  }
-
-  public function Print($value='')
-  {
-    // code...
-  }
-
-  public function Details($value='')
-  {
-    // code...
-  }
-
-  public function ListMantto($value='')
-  {
-    // code...
-=======
     $this->load->model(array('Mantto_model'));
   }
 
