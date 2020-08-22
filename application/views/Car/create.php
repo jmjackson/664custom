60c71255c5535e7af99bf72aedcf19a017e3ff01
miguelitho137 <66848704+miguelitho137@users.noreply.github.com>
2020-08-21 22:44:26 -0700

    update car

--- application/views/Car/create.php
+++ application/views/Car/create.php
@@ -6,15 +6,15 @@
                     <div class="" role="document">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">Agregar Auto - Folio No <?php echo $id ?></h5>
+                          <h5 class="modal-title" id="exampleModalLabel">Agregar Auto - Folio No <?php echo $fila->Folio; ?></h5>
                           </button>
                         </div>
                           <div class="modal-body">
-                            <form action="<?php echo base_url(); ?>Car/create" method="post">
+                            <form action="<?php echo base_url(); ?>Car/addcar" method="post">
                               <div class="row">
                                 <div class="col-md-6 pr-2">
                                   <div class="form-group">
-                                    <input type="hidden" name="ManttoId" class="form-control" placeholder="" value="<?php echo $manttoid ?>" readonly>
+                                    <input type="hidden" name="ManttoId" class="form-control" placeholder="" value="<?php echo $manttoid ?>">
                                   </div>
                                 </div>
                               </div>
@@ -50,7 +50,7 @@
                                 <div class="col-md-6 pr-1">
                                   <div class="form-group">
                                     <label>Año</label>
-                                    <input type="text" name="Model" class="form-control" placeholder="" value="" >
+                                    <input type="text" name="Year" class="form-control" placeholder="" value="" >
                                   </div>
                                 </div>
                               </div>
@@ -58,7 +58,7 @@
                                 <div class="col-md-12">
                                   <div class="form-group">
                                     <label>Descripción</label>
-                                    <textarea class="form-control textarea" name="Description">
+                                    <textarea class="form-control textarea" name="Notes">
                                     </textarea>
                                   </div>
                                 </div>
