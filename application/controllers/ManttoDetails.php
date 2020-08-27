039f825f718b7cb422d533e4c42847b506c60ff4
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-08-26 22:51:56 -0700

    Push Servicios

--- application/controllers/ManttoDetails.php
+++ application/controllers/ManttoDetails.php
@@ -7,15 +7,50 @@ class ManttoDetails extends CI_Controller{
   {
     parent::__construct();
     //Codeigniter : Write Less Do More
+    $this->load->model(array('Service_model','Mantto_model','ManttoDetails_model'));
   }
 
-  
+
 
   public function addDetails($id='')
   {
+    $datos = array(
+      'services' =>$this->Service_model->GetServices() ,
+      'Mantto'=>$this->Mantto_model->GetManttoId($id),
+      'MD'=>$this->ManttoDetails_model->GetServices($id),);
+
     $data = array('title' =>"Agregar Servicios" , );
     $this->load->view('Layouts/head', $data);
-    $this->load->view('ManttoDetails/add');
+    $this->load->view('ManttoDetails/add',$datos);
     $this->load->view('Layouts/footer');
+  }
+
+  public function AddMantto()
+  {
+    $manttoId=$this->input->post('ManttoId');
+    $serviceId=$this->input->post('ServiceId');
+    $hours=$this->input->post('Hours');
+    $rate=$this->input->post('Rate');
+    $total=$hours*$rate;
+    $status="Pendiente";
+
+    $datos = array(
+      'ManttoId' =>$manttoId ,
+      'ServiceId'=>$serviceId,
+      'Hours'=>$hours,
+      'Rate'=>$rate,
+      'Total'=>$total,
+      'Status'=>$status,
+    );
+
+    if ($this->ManttoDetails_model->Add($datos)) {
+      redirect(base_url().'ManttoDetails/addDetails/'.$manttoId);
+    }
+    else{
+      echo "Tienes un error";
+    }
+
+
+
   }
 }
---
+++ application/models/ManttoDetails_model.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ManttoDetails_model extends CI_Model{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+  public function Add($datos)
+  {
+    return $this->db->insert('ManttoDetail', $datos);
+  }
+
+  public function GetServices($id)
+  {
+    $this->db->select('md.Hours,md.Rate,md.Total,s.Name,sp.Name Supplier');
+    $this->db->from('ManttoDetail md');
+    $this->db->join('Services s', 'md.ServiceId = s.Id', 'inner');
+    $this->db->join('Suppliers sp', 's.SupplierId = sp.Id', 'inner');
+    $this->db->where('md.ManttoId', $id);
+    return $this->db->get()->result();
+  }
+}
