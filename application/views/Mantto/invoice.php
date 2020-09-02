45787c95ac389ddcdbadac3af953673c8bc797fe
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-09-02 10:45:12 -0700

    update invoice

--- application/views/Mantto/invoice.php
+++ application/views/Mantto/invoice.php
@@ -10,7 +10,13 @@
         <div class="col-md-12">
           <div class="card">
             <div class="card-header">
-
+              <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                      
+                    </div>
+                </div>
+              </div>
             </div>
             <div class="card-body">
 
